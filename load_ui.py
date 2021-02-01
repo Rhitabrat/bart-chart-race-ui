cedc8790593fa1bbc6644cbfff84365c6df61180
rhitabrat <pokharelrhitabrat@gmail.com>
2021-02-01 17:55:04 +0545

    buttons added

--- load_ui.py
+++ load_ui.py
@@ -1,17 +1,37 @@
 from tkinter import *
 import os
+from tkinter import filedialog
 
-window = Tk()
+class BCR_UI:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Bar Chart Race")
+        self.window.geometry("500x500")
+        self.window.configure(bg="#CDDDFD")
+    
+    def createVideo(self):
+        os.system('python3 test.py')
 
-window.title("Bar Chart Race")
+    def runButton(self):
+        btn = Button(self.window, text="Click Me", command=self.createVideo)
+        btn.grid(row=2, column=0)
 
+    def browseData(self):
+        file = filedialog.askopenfilename(filetypes = (("CSV Files","*.csv"),))
+        label = Label(self.window, text=os.path.basename(os.path.normpath(file)))
+        label.grid(row=1, column=0)
+        return file
 
+    def uploadAction(self):
+        btn = Button(self.window, text="Browse", command=self.browseData)
+        btn.grid(row=0, column=0)
 
-def createVideo():
-    os.system('python3 main.py')
+    def execution(self):
+        self.uploadAction()
+        self.runButton()
+        self.window.mainloop()
 
+    
 
-btn = Button(window, text="Click Me", command=createVideo)
-btn.grid(column=0, row=0)
-
-window.mainloop()
+bcr = BCR_UI()
+bcr.execution()
